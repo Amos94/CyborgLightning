22c36d1ec621cdaaf5788b41ff49675aafed763c
gurunish <nishant.gurung@kcl.ac.uk>
2016-02-15 16:49:13 +0000

    Added SharedPref code to allow automated log-in.

--- app/src/main/java/com/example/amosmadalinneculau/objects/MainActivity.java
+++ app/src/main/java/com/example/amosmadalinneculau/objects/MainActivity.java
@@ -4,20 +4,14 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
-import java.sql.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,12 +29,24 @@ public class MainActivity extends AppCompatActivity {
     //dialog
     public AlertDialog.Builder builder;
 
+    private static final String myPref = "MyPref";
+    private SharedPreferences settings;
+    private SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //setContentView(R.layout.activity_main_menu);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        settings = getSharedPreferences(myPref, Context.MODE_PRIVATE);
+        editor = settings.edit();
+        Boolean isSaved = settings.getBoolean("save",false);
+
+        if(isSaved){
+            Intent intent = new Intent(this, MainMenu.class);
+            startActivity(intent);
+        } else {
+            setContentView(R.layout.activity_main);
+            //setContentView(R.layout.activity_main_menu);
+            //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 /*        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -52,20 +58,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });*/
 
-        email = (EditText) findViewById(R.id.user_email);
-        password = (EditText) findViewById(R.id.password_textfield);
+            email = (EditText) findViewById(R.id.user_email);
+            password = (EditText) findViewById(R.id.password_textfield);
 
+            //dialog for login
+            builder = new AlertDialog.Builder(this);
 
-        //dialog for login
-        builder = new AlertDialog.Builder(this);
-
-        builder.setMessage("Couldn't login")
-                .setTitle("There was a problem with your email or password.");
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                // User clicked OK button
-            }
-        });
+            builder.setMessage("Couldn't login")
+                    .setTitle("There was a problem with your email or password.");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    // User clicked OK button
+                }
+            });
+        }
     }
 
 
@@ -96,6 +102,12 @@ public class MainActivity extends AppCompatActivity {
      */
     public void goToRegisterScreen(View view) {
         Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+    }
+
+    public void goToMainScreen(View view){
+        //if log-in is successful then do this
+        Intent intent = new Intent(this, MainMenu.class);
         startActivity(intent);
     }
 
